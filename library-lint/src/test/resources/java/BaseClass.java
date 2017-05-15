966874d13c4b0b7925f6d24e6f6ae50b81b75fb9
Juncheng Chen <junchengc@evernote.com>
2017-05-15 14:38:18 -0700

    Add lint tests to android-state library

---
+++ library-lint/src/test/resources/java/BaseClass.java
@@ -0,0 +1,11 @@
+package com.evernote.android.state.lint;
+
+/**
+ * Created by junchengc on 3/29/17.
+ */
+public class BaseClass {
+  private boolean mBoolean;
+  protected String mString;
+  public static final boolean STATIC_BOOLEAN = true;
+  public static final String STATIC_STRING = "some junk";
+}
